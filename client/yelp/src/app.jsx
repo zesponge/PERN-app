a9485c51f69aae60040b2b09543b67f3b404878f
Mario Su <sumario82@gmail.com>
2023-01-26 12:00:36 -0500

    rendering db

--- client/yelp/src/app.jsx
+++ client/yelp/src/app.jsx
@@ -3,17 +3,22 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import RestaurantDetailPage from './routes/RestaurantDetailPage';
 import UpdatePage from './routes/UpdatePage';
 import Home from './routes/Home';
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 
 const App = () => {
-    return <div>
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<Home/>} />
-                <Route exact path="/restaurants/:id/update" element={<UpdatePage/>}/>
-                <Route exact path="/restaurants/:id" element={<RestaurantDetailPage/>}/>
-            </Routes>
-        </Router>
-    </div>
+    return (
+        <RestaurantsContextProvider>
+            <div className='container'>
+                <Router>
+                    <Routes>
+                        <Route exact path="/" element={<Home/>} />
+                        <Route exact path="/restaurants/:id/update" element={<UpdatePage/>}/>
+                        <Route exact path="/restaurants/:id" element={<RestaurantDetailPage/>}/>
+                    </Routes>
+                </Router>
+            </div>
+        </RestaurantsContextProvider>
+    )
 };
 
 export default App;
